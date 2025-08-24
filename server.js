d04e30eea2824dfca8ae8e8664909773d8c475de
Douglas Ramirez <marlonc@tevixmd.com>
2025-08-24 14:48:40 -0600

    Agregar colores y opcion de tirar dados

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'desempate.html'));
+});
+
+
 app.get('/triangular', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'triangular.html'));
 });
